b4634248bb82df2c289bfe7363ce101b8a9eb9d2
Tammo Hesselink <84312138+tammohesselink@users.noreply.github.com>
2023-11-18 18:39:30 +0000

    Feat/add m3u playlist (#5)
    * Added to readme
    
    * Added additional documentation to readme
    
    * Added functionality
    
    * Added error logging
    
    * Fixed bit depth reader
    
    * Lint
    
    * Added m3u playlist creation
    
    * Linting

---
+++ audio_conversion_tools/rekordbox/create_m3u_playlist.py
@@ -0,0 +1,9 @@
+from audio_conversion_tools.logging import logger
+
+
+def create_m3u_playlist(files, playlist_location):
+    with open(playlist_location, "w") as playlist_file:
+        for file in files:
+            playlist_file.write("/" + file.Location + "\n")
+
+    logger.info(f"M3U playlist '{playlist_location}' has been created.")
