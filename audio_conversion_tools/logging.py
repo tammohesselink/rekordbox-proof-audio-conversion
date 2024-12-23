54db9a9b68b6a613910aefe5698fce1f37f7f4cd
tammohesselink <84312138+tammohesselink@users.noreply.github.com>
2024-12-23 12:36:36 +0100

    Migrate to ruff and uv

--- audio_conversion_tools/logging.py
+++ audio_conversion_tools/logging.py
@@ -1,11 +1,16 @@
 import sys
 
-from loguru import logger
+from loguru import logger as _logger
 
 # Remove the default sink (console)
-logger.remove()
-logger.add(
+_logger.remove()
+_logger.add(
     sys.stdout,
     format="<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | <level>{level: <8}</level> | <level>{message}</level>",
     colorize=True,
 )
+
+# Explicitly export the logger
+logger = _logger
+
+__all__ = ["logger"]
